29fbde98648044189a9dfe0719995ebb61494014
Monica Ular <moniq.iancu@gmail.com>
2024-03-13 17:25:07 +0000

    Add a new model to the database

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-monicaular-myfirstdjang-hsvdlmwiw5h.ws-eu108.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-monicaular-myfirstdjang-hsvdlmwiw5h.ws-eu110.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
