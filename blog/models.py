c288ff1222f59f3e9e144ae0e66f19974855710b
Monica Ular <moniq.iancu@gmail.com>
2024-03-16 15:52:21 +0000

    add pagination settings

--- blog/models.py
+++ blog/models.py
@@ -35,7 +35,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    challenge = models.SlugField(max_length=200, unique=True, default="insert rating")
+   
 
     class Meta:
         ordering = ["created_on"]
